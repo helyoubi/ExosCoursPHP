d27ea8b3422f0a80280fe6fa7c10885a4b9d61e4
Elyoubi Hamza <hamzaelyoubi1993@gmail.com>
2017-04-22 15:18:24 +0200

    Ajout de la page php de conditions

---
+++ phpBegin/Condition.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hp
+ * Date: 22/04/2017
+ * Time: 15:06
+ */
+
+$variableNULL = true;
+
+if(!$variableNULL){
+
+    echo 'Casse toi !';
+
+}else{
+
+    echo 'Bienvenue';
+
+}
