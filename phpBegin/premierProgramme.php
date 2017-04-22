612b5de64d0830036f84f33b7f5b86daab6a8586
Elyoubi Hamza <hamzaelyoubi1993@gmail.com>
2017-04-22 10:19:38 +0200

    Apprentisage des bases , affichages , commentaires ..

--- phpBegin/premierProgramme.php
+++ phpBegin/premierProgramme.php
@@ -1,10 +1,14 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Hp
- * Date: 21/04/2017
- * Time: 21:19
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Ceci est une page de test il est <?php echo date('h:i:s'); ?></title>
+</head>
+<body>
 
-echo 'Bonjour KOKO';
+<h1>Ma page web</h1>
 
+<p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+
+</body>
+</html>
