0500ba8f62a80d195324d5fd36fc19553675221e
Elyoubi Hamza <hamzaelyoubi1993@gmail.com>
2017-04-22 11:45:25 +0200

    Ajour partie des variables

---
+++ phpBegin/Variables.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hp
+ * Date: 22/04/2017
+ * Time: 11:33
+ */
+
+$ageVisiteur = 24;
+
+$estcevrai = true;
+
+//echo $ageVisiteur;
+
+echo 'Wé c\'est'. $ageVisiteur. 'ans';
+
+$prix = 10;
+
+$cout = 2.5;
+
+$total = $prix*$cout;
+
+echo '<br>Le prix total est '.$total.' Euros';
