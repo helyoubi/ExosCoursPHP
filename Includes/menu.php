50a1436d4c793aa537e4aad2b1b4a35adaf4c9b7
Elyoubi Hamza <hamzaelyoubi1993@gmail.com>
2017-04-23 12:56:31 +0200

    Apprentissage des includes en php

---
+++ Includes/menu.php
@@ -0,0 +1,10 @@
+<nav id="menu">
+    <div class="element_menu">
+        <h3>Titre menu</h3>
+        <ul>
+            <li><a href="#">Lien 1</a></li>
+            <li><a href="#">Lien 2</a></li>
+            <li><a href="#">Lien 3</a></li>
+        </ul>
+    </div>
+</nav>
