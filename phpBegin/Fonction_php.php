52d92ac611ca93813bb3cebd1ced7977a6a7b65c
Elyoubi Hamza <hamzaelyoubi1993@gmail.com>
2017-04-23 11:03:36 +0200

    Ajout de la page de fonction

---
+++ phpBegin/Fonction_php.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hp
+ * Date: 23/04/2017
+ * Time: 10:35
+ */
+
+function calculCube($val){
+
+    return $res = $val*$val*$val;
+
+}
+
+
+$volume = calculCube(4);
+
+echo $volume;
