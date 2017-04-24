b20685ce0a877db4fe01ae3b9fd8cb861fae3e44
Elyoubi Hamza <hamzaelyoubi1993@gmail.com>
2017-04-24 12:18:21 +0200

    Apprentissage transmission de données en php $_GET et Transtipage

---
+++ phpBegin/Transmettre_donnee_php.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hp
+ * Date: 24/04/2017
+ * Time: 09:55
+ */
+
+?>
+
+<a href="bonjour.php?nom=Dupont&amp;prenom=Jean&amp;repeter=14" target="_blank">Dis-moi bonjour !</a>
+
+
---
+++ phpBegin/bonjour.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<h4>T'es dans la page bonjour.html mon poteau !!</h4>
+
+<?php
+
+
+    //La fonction isset() permet de vérifier si une variable est définie ou non
+    if(isset($_GET['nom']) && isset($_GET['nom']) && isset($_GET['repeter'])) {
+
+        //Transtipage : convertion en int !
+
+        $_GET['repeter'] = (int) $_GET['repeter'];
+
+        if($_GET['repeter']>0 && $_GET['repeter']<=100){
+
+    for($i=0;$i<$_GET['repeter'];$i++) {
+
+     ?>
+
+
+        <p>Bonjour <?php echo $_GET['prenom'] . ' ' . $_GET['nom']; ?> !</p>
+
+      <?php
+
+
+    }
+
+
+    }else if($_GET['repeter']==0){
+
+            echo 'La valeur de répéter n\'est pas valide !';
+        }
+
+    }else{
+
+    echo 'Vous avez supprimez les paramètres !';
+
+    }
+
+?>
+
+
+</body>
+
+</html>
