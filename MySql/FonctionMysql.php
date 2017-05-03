820e33948edbbc974b409dc9e54a3664393a3bdc
Elyoubi Hamza <hamzaelyoubi1993@gmail.com>
2017-05-03 11:37:34 +0200

    Fonction de date , NOW() , date et dateTime en SQL

--- MySql/FonctionMysql.php
+++ MySql/FonctionMysql.php
@@ -6,3 +6,20 @@
  * Time: 20:26
  */
 
+try{
+
+
+    $connect = new PDO('mysql:host=localhost;dbname=parcoursphp;charset=utf8','root','');
+
+}catch (Exception $e){
+
+    die("Erreur !  : ".$e->getMessage());
+}
+
+
+//Utilisation de la fonction SQL NOW() , qui retourne la date du jour 
+$req = $connect->exec('INSERT INTO jeux_video(nom, possesseur, console, prix, nbre_joueurs_max, commentaires,date_ajout) VALUES(\'aizdojd\', \'aozidji\', \'aozidji\', 23.50, 16, \'aozidji\',NOW())');
+
+
+
+echo 'Le jeu a bien été ajouté ';
